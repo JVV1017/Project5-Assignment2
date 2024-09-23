19471a9ee0eba950cc0c6af7d7007a29c494f7cf
BATVET1017 <122996558+BATVET1017@users.noreply.github.com>
2024-09-22 23:12:40 -0400

    PreRelease Step 5

--- Assignment2/source.cpp
+++ Assignment2/source.cpp
@@ -9,6 +9,11 @@ using namespace std;
 struct STUDENT_DATA {
 	string firstName;
 	string lastName;
+
+	// Email struct member if executable running is in PreRelease version
+#ifdef PRE_RELEASE
+		string email;
+#endif
 };
 
 // Function to parse the data and return the STUDENT_DATA object
@@ -22,16 +27,32 @@ STUDENT_DATA parseStudentData(const string& line) {
 	// Parse the last names
 	getline(ss, student.lastName, ',');
 
+	// Parse the email addresses of students in pre-release mode
+#ifdef PRE_RELEASE	
+	getline(ss, student.email, ',');
+#endif
+
 	return student;
 }
 
 int main() {
 
+	// Prints on screen whether if the app running is PreRelease or Standard version
+#ifdef PRE_RELEASE
+		cout << "Running the Pre-Release version of the software." << endl;
+#else
+		cout << "Running the Standard version of the software." << endl;
+#endif
+
 	// Creates a vector based object of the struct object students
 	vector<STUDENT_DATA> students;
 
-	// Reads the file, StudentData.txt
-	ifstream inFile("StudentData.txt");
+	// Reads the file, StudentData.txt if in Standard or StudentData_Emails if in PreRelease
+#ifdef PRE_RELEASE
+		ifstream inFile("StudentData_Emails.txt");
+#else 
+		ifstream inFile("StudentData.txt");
+#endif
 
 	// If file doesn't open, prints message that it doesn't open
 	if (!inFile) {
@@ -53,11 +74,22 @@ int main() {
 	inFile.close();
 
 #ifdef _DEBUG
-
 	// TO output the students data for debugging
 	cout << "Debug mode: printing student information:" << endl;
 
 	// Output the student for verification
+	for (const auto& student : students) {
+		cout << "First Name: " << student.firstName << ", Last Name: " << student.lastName << endl;
+	}
+#endif
+
+#ifdef PRE_RELEASE 
+	// Output the complete student details in the PreRelease version
+	for (const auto& student : students) {
+		cout << "First Name: " << student.firstName << ", Last Name: " << student.lastName << ", Email: " << student.email << endl;
+	}
+#else
+	// Output the student details without emails in the Standard version
 	for (const auto& student : students) {
 		cout << "First Name: " << student.firstName << ", Last Name: " << student.lastName << endl;
 	}
